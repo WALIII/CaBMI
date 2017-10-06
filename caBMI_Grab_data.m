3f9ba037a9744db21b1ed2367c867b0e0d442cc5
WALIII <bliberti@bu.edu>
2017-10-05 21:38:23 -0700

    re-arrange functions

--- caBMI_Grab_data.m
+++ caBMI_Grab_data.m
@@ -1,43 +1,42 @@
 function caBMI_Grab_data()
-
 % grab pixel values and do perform basic operations on them
 
 
 pl = actxserver('PrairieLink.Application');
-
 pl.connect();
 
-Im1 = pl.GetImage(1);
-% GetImage_2(channel,pixelsPerLine,LinesPerFrame);
 
-figure(); 
-imagesc(Im1); colormap(bone);
+
+% Collect Baseline Data:
+
+for i = 1:100;
+Im1(:,:,i) = pl.GetImage(1);
+end
+
+
+figure();
+imagesc(std(Im1,[],3)); colormap(bone);
 colorbar
 
 
 % the four cells
-roi_1
-roi_2
-roi_3
-roi_4
+ROIs = caBMI_SelectROIs(image,4)
 
-% to do: function that computes running baseline and 
-[baseline]_ caBMI_GetBase(rois)
 
-group_1 = roi_1+roi_2;
+% to do: function that computes running baseline and
+[baseline] caBMI_GetBase(rois)
 
-baseline(percentile(group_1,
 
+kk = pl.ReadRawDataStream(1);
 
 % for i = 1:100000
 % tic;
-% kk = pl.ReadRawDataStream(1);
+
 % H(i) = toc;
 % end
 
-figure();
-histogram(H);
 
 
-% Read the raw data stream 
+
+% Read the raw data stream
 % ReadRawDataStream()
---
+++ caBMI_SelectROIs.m
@@ -0,0 +1,4 @@
+function [ROIs] = caBMI_SelectROIs(images,num)
+% caBMI_SelectROIs
+
+% Have user Select ROIs, and export the pixel values
