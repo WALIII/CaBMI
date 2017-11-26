0e14516ad2a69967838fde7aea8df63d8a8d7cb8
WALIII <bliberti@bu.edu>
2017-11-26 13:58:10 -0800

    update integrated scrip

--- test_scrap.m
+++ test_scrap.m
@@ -1,8 +1,12 @@
+% Test Serial Port Setup for arduino
+% WAL3
+% d11.26.17
+
 clear all
 clc
  
 answer=1; % this is where we'll store the user's answer
-arduino=serial('/dev/tty.usbmodem1421','BaudRate',9600); % create serial communication object on port COM4
+arduino=serial('COM12','BaudRate',9600); % create serial communication object on port COM4
  
 fopen(arduino); % initiate arduino communication
  counter = 1;
@@ -10,7 +14,7 @@ fopen(arduino); % initiate arduino communication
 while answer
     
     answer=input('Enter led value 1 or 2 (1=ON, 2=OFF, 0=EXIT PROGRAM): '); % ask user to enter value for variable answer
-    fprintf(arduino,'%c', counter); % send answer variable content to arduino
+    fprintf(arduino,'%c', answer); % send answer variable content to arduino
 counter = counter+1;
 end
 
