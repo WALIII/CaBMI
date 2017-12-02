6ae0dbbff805bedcfabfea5fb37f4d9ca3363303
WALIII <bliberti@bu.edu>
2017-12-01 18:35:12 -0800

    part 2

---
+++ CarmenaDailyGrind/CarmenaDailyGrind_P02.m
@@ -0,0 +1,32 @@
+function CarmenaDailyGrind_P02
+
+
+  % CarmenaDailyGrind.m  Run all subroutenes
+  % CarmenaDailyGrind_P01 % Pull data from 2P computer
+  % CarmenaDailyGrind_P02 % Basic video alignment and data integrity check
+  % CarmenaDailyGrind_P03 % Automatic ROI extraction and alignment with cursors
+
+  % AR_DataTransfer
+
+  % Transfer Data from Multiphoton Computer every evening at midnight, and run
+  % analysis on the aquired DATA
+
+  %   Created: 2017/12/01
+  %   By: WALIII
+  %   Updated: 2017/12/01
+  %   By: WALIII
+
+
+
+  % Format:
+
+  % C:\Data\Imaging Data\d120117\trial01\(*DATA* + *LOGS* + *SYNC*)
+
+  % Now, a .m file with:
+  %    1. Video processing offsets
+  %    2. E1 and E2 cell ( cursor)
+  %    3. Triggers matrix ( frame triggers, )
+  %    4. ...
+
+
+  
