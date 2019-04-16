f766e6fe372fceec51b591afde33273a20297a2d
WALIII <bliberti@bu.edu>
2019-04-15 21:19:29 -0700

    push for dtw

--- Analysis/CaBMI_SequenceEmerge.m
+++ Analysis/CaBMI_SequenceEmerge.m
@@ -15,7 +15,7 @@ RS = 1:(round((size(ROIhits,1)/3))*3)-1;
 
 % Get top cells:
 
-[ROIhits_z2 ROIhits_z] = CaBMI_topCells(ROIhits(:,:,:),rn,0.7);
+[ROIhits_z2 ROIhits_z] = CaBMI_topCells(ROIhits(:,:,:),rn,0.9);
 
 
 G1 = ROIhits_z;
