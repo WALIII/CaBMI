c0c1dde90766f777e78dc260f0fd5928a786bc21
WALIII <bliberti@bu.edu>
2019-11-12 13:29:44 -0800

    add folder and start ScanImage migration

---
+++ Acquisition/ScanImage/ caBMI_Main_SI.m
@@ -0,0 +1,5 @@
+% caBMI_Main_SI.m
+
+% Walkthorugh of the basic functions of the BMI setup.
+% grab pixel values  under user selected Regions of interest
+% and perform numerical operations on them.
