94ac169bfa49d63360a429a02c0a3b5e60c5e071
WALIII <bliberti@bu.edu>
2020-02-28 15:14:04 -0800

    update syntax for SI

--- Acquisition/ScanImage/CaBMI_SI_BMI.m
+++ Acquisition/ScanImage/CaBMI_SI_BMI.m
@@ -1,7 +1,52 @@
 function CaBMI_SI_BMI
+% BMI Scripts for Scanimage
 
-clear BMI_Data;
-BMI_Data.time = 1;
-BMI_Data.Frame = zeros(515,512)
-counter = 1; Tstart = tic; 
-out =  automatedGrab_BMI();
+
+% ask to generate new folder ( animal nams)
+
+PATH = 'C:\Users\User\Documents\MATLAB'; % Make sure this is unique!
+
+% conncet to Arduino Through Serial
+arduino=serial('COM13','BaudRate',9600); % create serial communication object on port COM4
+fopen(arduino); % initiate arduino communication
+
+
+% Initialize workspace variables
+BMI_Data.time = [];
+BMI_Data.Frame = zeros(515,512);
+counter = 1;
+Tstart = tic; % timing vector
+
+%% BASELINE
+filename = 'Baseline_BEFORE'
+BMI_Data.BMIready = 0;
+time2run = 10; % minutes
+TotalFrames = 30*60*time2run;
+automatedGrab_BMI(TotalFrames,PATH,filename);
+
+% save Baseline
+BMI_Data_Baseline = BMI_Data;
+
+%clear workspace
+clear BMI_Data Tstart counter
+
+%% Pick Cells ( or load from previous run)
+
+% load baseline
+% pick cells
+
+%% RUN BMI
+filename = 'BMI'
+BMI_Data.BMIready = 1;
+time2run = 60; % minutes
+TotalFrames = 30*60*time2run;
+automatedGrab_BMI(TotalFrames,PATH);
+
+%% POST EXPERIMENT BASELINE
+filename = 'Baseline_AFTER'
+BMI_Data.BMIready = 0;
+time2run = 10; % minutes
+TotalFrames = 30*60*time2run;
+automatedGrab_BMI(TotalFrames,PATH,filename);
+
+% save data
--- Acquisition/ScanImage/myFrameAcquiredCallback_BMI.m
+++ Acquisition/ScanImage/myFrameAcquiredCallback_BMI.m
@@ -3,9 +3,10 @@ tic
 hSI = src.hSI; % get the handle to the ScanImage model
 
 % Load in data from Workspace:
-counter = evalin('base','counter'); % TO DO add this 'frame number' to BMI_Data 
+counter = evalin('base','counter'); % TO DO add this 'frame number' to BMI_Data
 Tstart = evalin('base','Tstart');
 BMI_Data = evalin('base','BMI_Data');
+arduino = evalin('base','arduino');
 
 % scanimage stores image data in a data structure called 'stripeData'
 % this example illustrates how to extract an acquired frame from this structure
@@ -16,6 +17,7 @@ for idx = 1:length(channels)
     frame{idx} = lastStripe.roiData{1}.imageData{idx}{1}; % extract all channels
 end
 
+if BMI_Data.BMIready ==1; % if BMI is ready to go:
 % Index into E1-E4;
 
 
@@ -28,8 +30,12 @@ end
 % Calculate Cursor
 
 
+% Send serial command
 %disp(['FRAME ', num2str(counter),' Acquired'])
 
+else
+  
+end
 
 BMI_Data.time(counter) = toc(Tstart);
 BMI_Data.cursor(counter) = mean(frame{1}(1,:));
