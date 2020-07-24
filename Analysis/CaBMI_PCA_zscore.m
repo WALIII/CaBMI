a1e703d23c1228e9d428184296581ca569279c0f
WALIII <bliberti@bu.edu>
2020-07-24 10:46:42 -0700

    update batch plotting

--- Analysis/CaBMI_PCA_zscore.m
+++ Analysis/CaBMI_PCA_zscore.m
@@ -1,4 +1,4 @@
-function [out] = CaBMI_PCA_zscore(ROIhits_z,Nmax);
+function [out pc1exp] = CaBMI_PCA_zscore(ROIhits_z,Nmax);
 
 
 % Nmax = 95;
@@ -13,6 +13,7 @@ for i = 1:size(ROIhits_z,1);
 warning off
 [coeff,score,latent,tsquared,explained,mu] = pca(squeeze(AA));
 A = explained;
+pc1exp(i) = explained(1);
 ca = cumsum(A);  % Sum them all up
 lastIndex = find(ca <= Nmax, 1, 'last'); % Last index before the sum would exceed Nmax.
 % Show what elements they were "Pending" (whatever that means in this context).
@@ -21,5 +22,8 @@ clear AA AA2 ca A lastIndex
 end
 
 figure(); plot(out,'*');
+title(['Number of dimensions to explain ', num2str(Nmax),' percent variance']);
 
+figure(); plot(pc1exp,'*');
+title(['Percent variance explained by first PC']);
 
